d0a3e4fb915421c853750a7a86009bafb3d0ec83
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-01-21 19:31:00 +0700

    Menambahkan fetch data untuk tabel pada HTML, CSS, dan JavaScript

--- js/script.js
+++ js/script.js
@@ -1,41 +1,75 @@
-let frame = (data) => {
-  // Menambahkan data pada tabel
-  // return `
-  //   <tr>
-  //     <td>${data.name}</td>
-  //     <td>${data.site}</td>
-  //     <td>${data.start_time}</td>
-  //     <td>${data.end_time}</td>
-  //     <td>${data.url}</td>
-  //   </tr>
-  // `
+const content = document.querySelector("tBody")
 
-  // Menambahkan data pada dropdown
-  $(document).ready(function () {
-    $('.js-data-example-ajax').select2({
-      data: data
-    });
+const frame = (datas) => {
+  let node = ''
+  datas.forEach(data => {
+    node += `
+    <tr>
+    <td>${data.name}</td>
+    <td class="text-center">${data.birth_year}</td>
+    <td class="text-center">${data.death_year}</td>
+    <td class="text-center">${data.ascension_year}</td>
+    <td>${data.description}</td>
+    </tr>
+    `
   });
+  content.innerHTML = node
 }
+
+const search = document.querySelector("#search")
 
 fetch('https://indonesia-public-static-api.vercel.app/api/heroes', {
   method: "GET"
 }).then(response => response.json()).then(data => {
   // console.log(data)
 
-  // Menambahkan data pada tabel
-  // let node = ''
-  // data.forEach(element => {
-  //   // frame(element)
-  // });
-  // content.innerHTML = node
+  // Pilihan
+  const pilih = document.querySelector('.pilihan')
+  pilih.addEventListener('change', (e) => {
+    // console.log(e.target.value);
 
-  // Menambahkan data pada dropdown
+    let pilihan = e.target.value.toLowerCase()
+
+    switch (pilihan) {
+      case "dropdown":
+        document.querySelector(".dropdown").style.display = "block"
+        document.querySelector(".table").style.display = "none"
+        break;
+        case "table":
+          document.querySelector(".table").style.display = "block"
+          document.querySelector(".dropdown").style.display = "none"
+        break;
+      default:
+        break;
+    }
+  })
+
+  // Menambahkan data pada tabel
+  search.addEventListener("keyup", (e) => {
+    let keyword = e.target.value.toLowerCase()
+
+    const filterName = data.filter(function (data) {
+      const name = data.name.toLowerCase();
+      return name.indexOf(keyword) > -1;
+    });
+    // console.log(filterName);
+
+    frame(filterName)
+  })
+  frame(data)
+
   let arr = []
   for (let index = 0; index < data.length; index++) {
     // const element = data[index];
     arr.push({ id: index, text: data[index].name })
     // console.log(data[index]);
   }
-  frame(arr)
+
+  $(document).ready(function () {
+    $('.js-data-example-ajax').select2({
+      placeholder: 'Cari Nama...',
+      data: arr,
+      width: 'resolve'
+    });
+  });
 })
